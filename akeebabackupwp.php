b0304a3c47a1a35bc6be38bae5cf76415991433b
nikosdion <nikosdion@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-26 15:26:49 +0000

    WordPress.org, you are killing me
    git-svn-id: https://plugins.svn.wordpress.org/akeebabackupcore/trunk@1033225 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- akeebabackupwp.php
+++ akeebabackupwp.php
@@ -3,7 +3,7 @@
 Plugin Name: Akeeba Backup for WordPress
 Plugin URI: https://www.akeebabackup.com
 Description: The complete backup solution for WordPress
-Version: 4.1.0.rc5
+Version: 1.2.0.rc6
 Author: Akeeba Ltd
 Author URI: https://www.akeebabackup.com
 License: GPLv3
--- app/version.php
+++ app/version.php
@@ -1,4 +1,4 @@
 <?php
 define('AKEEBA_PRO', '0');
-define('AKEEBA_VERSION', '4.1.0.rc5');
+define('AKEEBA_VERSION', '1.2.0.rc6');
 define('AKEEBA_DATE', '2014-11-26');
